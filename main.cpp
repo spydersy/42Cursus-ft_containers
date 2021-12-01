e496a64f346867efdfd429d324add8b1c9378257
Achraf Belarif <abelarif@e2r5p7.1337.ma>
2021-12-01 18:37:24 +0100

    HOME ...

--- main.cpp
+++ main.cpp
@@ -1,34 +1,10 @@
 
-// #include <iostream>
-// #include <vector>
+#include "./vector/vector.hpp"
+#include <vector>
 
-// int     main()
-// {
-    // std::vector<std::string> vctr;
-
-    // vctr.push_back("a");
-    // vctr.push_back("b");
-    // vctr.push_back("c");
-    // vctr.push_back("c");
-    // vctr.push_back("c");
-    // std::cout << vctr.size() << std::endl;
-    // std::cout << vctr[2] << std::endl;
-    // std::cout << vctr.capacity() << std::endl;
-    // return (0);
-// }
-
-#include "./include/vector/vector.hpp"
-
-int     main()
+int main()
 {
-    try
-    {
-        ft::vector<std::string> v;
-    }
-    catch(const std::exception& e)
-    {
-        std::cerr << e.what() << '\n';
-    }
+    std::vector<int> v;
 
     return (0);
 }
